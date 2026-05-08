da4eb10c48893bbedce727a6c1f6d3822577ab0c
lisdanoviati <lisdanoviati36@gmail.com>
2026-05-08 13:04:54 +0700

    login berdasakan role ke dashboard

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,25 @@ Route::get('/', function () {
     return view('landing');
 });
 
+Route::get('/owner/dashboard', function () {
+    return view('owner.dashboard');
+})->middleware('auth');
+
+Route::get('/kasir/dashboard', function () {
+    return view('kasir.dashboard');
+})->middleware('auth');
+
+Route::get('/karyawan/dashboard', function () {
+    return view('karyawan.dashboard');
+})->middleware('auth');
+
 Route::get('/katalog', function () {
     return view('pelanggan.katalog');
 })->name('katalog');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
