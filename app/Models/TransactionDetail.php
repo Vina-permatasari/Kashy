28498f344126662e3b3fc5cfb20db253adc88944
nepann <naylavaniar@gmail.com>
2026-05-25 21:39:34 +0700

    Create TransactionDetail.php

---
+++ app/Models/TransactionDetail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TransactionDetail extends Model
+{
+    protected $table = 'transaction_details';
+    
+    protected $fillable = [
+        'transaction_id',
+        'product_id',
+        'qty',
+        'harga',
+        'subtotal'
+    ];
+    
+    protected $casts = [
+        'qty' => 'integer',
+        'harga' => 'integer',
+        'subtotal' => 'integer'
+    ];
+    
+    public function transaction(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+    
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
